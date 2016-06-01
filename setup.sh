31393c630a99b397b9074f6501ff286d68bd4b0c
takumi-no-kami <asano-y+github@acaric.co.jp>
2016-06-01 13:22:51 +0900

    add src

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 #
-# mobileapp-demo setup script
+# mobile-app-demo1 setup script
 #
+
+usage() {
+  echo "usage: $(basename $0) [-d <path>] [-w <URL>]"
+  echo "  -d <path>    application directory"
+  echo "  -w <URL>     hosted web-application URL"
+}
+
+
+## parameter settings
+_BASE_DIR=$(dirname $0)
+_APP_DIR=mobileapp-demo-1
+_WEBAPP_URL='https://enterprise.acaric.com/MobileHelloWorld/'
+
+while getopts "d:hw:" OPT
+do
+  case $OPT in
+    d ) _APP_DIR="$OPTARG" ;;
+    w ) _WEBAPP_URL="$OPTARG" ;;
+    h ) usage ; exit ;;
+    * ) echo "Invalid option: -$OPT" ; usage ; exit 1 ;;
+  esac
+done
