6e481a0d067c8a73c15e2524aa8337deb6e4f640
asano-y <sys_member@ACARIC-PC-86.local>
2016-06-02 02:39:16 +0900

    implement setup

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ usage() {
 
 
 ## parameter settings
-_BASE_DIR=$(dirname $0)
+_BASE_DIR=$(cd $(dirname $0); pwd)
 _APP_DIR=mobileapp-demo-1
 _WEBAPP_URL='https://enterprise.acaric.com/MobileHelloWorld/'
 
@@ -24,3 +24,46 @@ do
     * ) echo "Invalid option: -$OPT" ; usage ; exit 1 ;;
   esac
 done
+
+# check
+if [ ¥command -v cordova >/dev/null 2>&1 ]; then
+  echo '"cordova" command not found."'
+  exit 1
+fi
+cd _BASE_DIR
+if [ -d $_APP_DIR -o -f $_APP_DIR ]; then
+  echo "$_BASE_DIR/$_APP_DIR already exists."
+  exit 1
+fi
+
+# confirm
+echo " _APP_DIR = $_APP_DIR"
+echo " _WEBAPP_URL = $_WEBAPP_URL"
+echo 'OK? (y/n) >'
+read _ISOK
+if [ "$_ISOK" != 'y' ]; then
+ echo 'sure, setup exit.'
+ exit 0
+fi
+
+# source setup
+cordova create $_APP_DIR com.acaric.hellocordovaosp HelloCordovaOSP
+cd $_APP_DIR
+cordova platform add ios --save
+cordova platform add android --save
+mkdir res
+cd $_BASE_DIR
+_WEBAPP_HREF=$(echo $_WEBAPP_URL | awk -F/ '{print $1}')'//'$(echo $_WEBAPP_URL | awk -F/ '{print $3}')
+_FILES=$(find src -type f | cut -b5-)
+for _FILE in $_FILES
+do
+  if [ $(grep -l '_WEBAPP_[URL_|HREF_]' src/$_FILE) ]; then
+    sed "s*_WEBAPP_URL_*$_WEBAPP_URL*g" src/$_FILE | sed -e "s*_WEBAPP_HREF_*$_WEBAPP_HREF*g" > $_APP_DIR/$_FILE
+  else
+    cp src/$_FILE $_APP_DIR/$_FILE
+  fi
+done
+
+
+echo ''
+echo 'mobile-app-demo1 setup success! (^ - ^)'
